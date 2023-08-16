9cf49910cdea3055efa9306273402d70ab2c1c8e
_Csaramon <xyq162395@gmail.com>
2023-08-16 10:21:09 +0200

    Update PlotGroupERP.m

--- AnalysisFcn/PlotGroupERP.m
+++ AnalysisFcn/PlotGroupERP.m
@@ -72,8 +72,8 @@ pMap(tMap==0)=nan;
 lowFind = Para.freq <= 30;
 highFind = Para.freq > 30;
 % normalise
-y2plot = y2plot.*Para.freq;
-se2plot = se2plot.*Para.freq;
+y2plot = y2plot.*Para.freq.^2;
+se2plot = se2plot.*Para.freq.^2;
 
 % uncorrected
 highlight = double(pMap< 0.05);
@@ -92,8 +92,8 @@ highlight(highlight==0) = nan;
 highlightcorr(highlightcorr==0) = nan;
 
 % generete the main figure and specify the displaying style
-hf = figure('Units','centimeters','Position', [8 6 16 12]);
-set(gca,'linewidth',3,'FontSize',16)
+hf = figure('Units','centimeters','Position', [8 6 32 27]);
+set(gca,'linewidth',5,'FontSize',28,'FontName','Arial','InnerPosition',[0.11,0.11,0.78,0.82])
 hold on
 xlim([0 120])
 
@@ -101,29 +101,29 @@ xlim([0 120])
 yyaxis left
 set(gca,'ycolor','k')
 ylabel('Low Frequency Power (a.u.)')
-hlowM = shadedErrorBar(Para.freq(lowFind), y2plot(1,lowFind),se2plot(1,lowFind),{'color',[255 106 106]/255,'linewidth',1,'linestyle','-'},1);
-hlowS = shadedErrorBar(Para.freq(lowFind), y2plot(2,lowFind), se2plot(2,lowFind),{'color',[30 144 255]/255,'linewidth',1,'linestyle','-'},1);
+hlowM = shadedErrorBar(Para.freq(lowFind), y2plot(1,lowFind),se2plot(1,lowFind),{'color',[255 106 106]/255,'linewidth',1.5,'linestyle','-'},1);
+hlowS = shadedErrorBar(Para.freq(lowFind), y2plot(2,lowFind), se2plot(2,lowFind),{'color',[30 144 255]/255,'linewidth',1.5,'linestyle','-'},1);
 hlowS.mainLine.Marker = 'none';
 
-hlowsig = plot(Para.freq(lowFind),(max(y2plot(:,lowFind),[],'all')+0.2*range(y2plot(:,lowFind),'all'))*highlight(lowFind),'-','color',[0.5 0.5 0.5],'LineWidth',3);
-hlowsigcorr = plot(Para.freq(lowFind),(max(y2plot(:,lowFind),[],'all')+0.1*range(y2plot(:,lowFind),'all'))*highlightcorr(lowFind),'-','color',[0 0 0],'LineWidth',3);
+hlowsig = plot(Para.freq(lowFind),(max(y2plot(:,lowFind),[],'all')+0.2*range(y2plot(:,lowFind),'all'))*highlight(lowFind),'-','color',[0.5 0.5 0.5],'LineWidth',5);
+hlowsigcorr = plot(Para.freq(lowFind),(max(y2plot(:,lowFind),[],'all')+0.1*range(y2plot(:,lowFind),'all'))*highlightcorr(lowFind),'-','color',[0 0 0],'LineWidth',5);
 
 yyaxis right
 set(gca,'ycolor','k')
 ylabel('High Frequency Power (a.u.)')
-hhighM = shadedErrorBar(Para.freq(highFind), y2plot(1,highFind),se2plot(1,highFind),{'color',[255 106 106]/255,'linewidth',1,'linestyle','-'},1);
-hhighS = shadedErrorBar(Para.freq(highFind), y2plot(2,highFind), se2plot(2,highFind),{'color',[30 144 255]/255,'linewidth',1,'linestyle','-'},1);
+hhighM = shadedErrorBar(Para.freq(highFind), y2plot(1,highFind),se2plot(1,highFind),{'color',[255 106 106]/255,'linewidth',1.5,'linestyle','-'},1);
+hhighS = shadedErrorBar(Para.freq(highFind), y2plot(2,highFind), se2plot(2,highFind),{'color',[30 144 255]/255,'linewidth',1.5,'linestyle','-'},1);
 hhighS.mainLine.Marker = 'none';
 
-hhighsig = plot(Para.freq(highFind),(max(y2plot(:,highFind),[],'all')+0.2*range(y2plot(:,highFind),'all'))*highlight(highFind),'-','color',[0.5 0.5 0.5],'LineWidth',3);
-hhighsigcorr = plot(Para.freq(highFind),(max(y2plot(:,highFind),[],'all')+0.1*range(y2plot(:,highFind),'all'))*highlightcorr(highFind),'-','color',[0 0 0],'LineWidth',3);
+hhighsig = plot(Para.freq(highFind),(max(y2plot(:,highFind),[],'all')+0.2*range(y2plot(:,highFind),'all'))*highlight(highFind),'-','color',[0.5 0.5 0.5],'LineWidth',5);
+hhighsigcorr = plot(Para.freq(highFind),(max(y2plot(:,highFind),[],'all')+0.1*range(y2plot(:,highFind),'all'))*highlightcorr(highFind),'-','color',[0 0 0],'LineWidth',5);
 
 
 title(filename(1:end-4))
 xlabel('Frequency (Hz)')
 
 legend([hlowM.mainLine,hlowS.mainLine,hlowsig,hlowsigcorr], ...
-    ['Intact'],['Scrambled'],['P<0.05'],['P<0.05 (corrected)'],'box','off','NumColumns',2);
+    ['Intact'],['Scrambled'],['p<0.05'],['p<0.05 (corrected)'],'box','off','NumColumns',2);
 
 
 % plot the inlets of beta and gamma power of each participant
@@ -180,11 +180,11 @@ for isub = unique(lmeTBL.Sub)'
     
     ybetaM = mean(YcorrBeta(lmeTBL.Cond=='1' & lmeTBL.Sub==isub));
     ybetaS = mean(YcorrBeta(lmeTBL.Cond=='2' & lmeTBL.Sub==isub));
-    plot(haBeta,[1,2],[ybetaM,ybetaS],'-','Color',[subColor(double(isub),:) 0.25],'LineWidth',3)
+    plot(haBeta,[1,2],[ybetaM,ybetaS],'-','Color',[subColor(double(isub),:) 0.25],'LineWidth',5)
     
     ygammaM = mean(YcorrGamma(lmeTBL.Cond=='1' & lmeTBL.Sub==isub));
     ygammaS = mean(YcorrGamma(lmeTBL.Cond=='2' & lmeTBL.Sub==isub));
-    plot(haGamma,[1,2],[ygammaM,ygammaS],'-','Color',[subColor(double(isub),:) 0.25],'LineWidth',3)
+    plot(haGamma,[1,2],[ygammaM,ygammaS],'-','Color',[subColor(double(isub),:) 0.25],'LineWidth',5)
     
     ysubbetaM = [ysubbetaM,ybetaM];
     ysubbetaS = [ysubbetaS,ybetaS];
@@ -193,27 +193,27 @@ for isub = unique(lmeTBL.Sub)'
 end
 
 % plot mean value
-plot(haBeta,[1,2],[nanmean(ysubbetaM),nanmean(ysubbetaS)],'-','Color',[0.4 0.4 0.4 1],'LineWidth',3)
-set(haBeta,'LineWidth',3)
+plot(haBeta,[1,2],[nanmean(ysubbetaM),nanmean(ysubbetaS)],'-','Color',[0.4 0.4 0.4 1],'LineWidth',5)
+set(haBeta,'LineWidth',5)
 % a = get(haBeta,'Children');
 % set(haBeta,'Children',[a(2:end);a(1)])
-plot(haGamma,[1,2],[nanmean(ysubgammaM),nanmean(ysubgammaS)],'-','Color',[0.4 0.4 0.4 1],'LineWidth',3)
-set(haGamma,'LineWidth',3)
+plot(haGamma,[1,2],[nanmean(ysubgammaM),nanmean(ysubgammaS)],'-','Color',[0.4 0.4 0.4 1],'LineWidth',5)
+set(haGamma,'LineWidth',5)
 % a = get(haGamma,'Children');
 % set(haGamma,'Children',[a(2:end);a(1)])
 
 if lmeStatsBeta.pValue < 0.05
-    hsigBeta = plot(haBeta,1.5,max(get(haBeta,'ylim')),'k*','markersize',8);
-    legend([hsigBeta],['P<0.05'],'box','off');
+    hsigBeta = plot(haBeta,1.5,max(get(haBeta,'ylim')),'k*','markersize',12,'linewidth',2);
+    legend([hsigBeta],['p<0.05'],'box','off');
 end
 if lmeStatsGamma.pValue < 0.05
-    hsigGamma = plot(haGamma,1.5,max(get(haGamma,'ylim')),'k*','markersize',8);
-    legend([hsigGamma],['P<0.05'],'box','off');
+    hsigGamma = plot(haGamma,1.5,max(get(haGamma,'ylim')),'k*','markersize',12,'linewidth',2);
+    legend([hsigGamma],['p<0.05'],'box','off');
 end
-% save the figure to data location
-hf.Renderer = 'painters';
-printeps(hf,[pathname filename(1:end-4)])
 
+% save the figure to data location
+% hf.Renderer = 'painters';
+% printeps(hf,[pathname filename(1:end-4)])
 
 % set(gco,'YData',get(gco,'YData')+22.5)
 
